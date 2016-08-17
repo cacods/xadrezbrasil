777e8571f23927495476b4dc45dabfd600f81733
caco <caco@member.fsf.org>
2016-08-17 15:43:35 -0300

    Adiciona Bootstrap CDN para CSS

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <title>Xadrez Brasil</title>
 </head>
 <body>
-    @yield('content')
+    <div class="content">
+        @yield('content')
+    </div>
 </body>
 </html>
