36965a1f205cddf00f23db760ea0d657ffe0f326
caco <caco@member.fsf.org>
2016-08-17 16:26:55 -0300

    View apenas com a tabela, sem estilos

--- resources/views/tabuleiro.blade.php
+++ resources/views/tabuleiro.blade.php
@@ -1,5 +1,16 @@
 @extends('app')
 
 @section('content')
-    <h1>O Tabuleiro de Xadrez</h1>
+    <div class="tabuleiro">
+        <table>
+            <?php const SIZE = 8; ?> <!-- it's 8 x 8 cels -->
+            @for($i = 0; $i < SIZE; $i++)
+                <tr>
+                @for($j = 0; $j < SIZE; $j++)
+                    <td></td>
+                @endfor
+                </tr>
+            @endfor
+        </table>
+    </div>
 @endsection
