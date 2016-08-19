b6523c3f680b58b33cb958bc3e8637cc3a98bb83
caco <caco@member.fsf.org>
2016-08-19 09:57:26 -0300

    Melhora view

--- resources/views/chessboard.blade.php
+++ resources/views/chessboard.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
     <table class="chessboard">
-        <?php const SIZE = 8; ?> <!-- chessboard has 8 x 8 cells -->
-        @for($i = 0; $i < SIZE; $i++)
+        <!-- chessboard has 8 files x 8 ranks -->
+        <?php const FILES = 8; const RANKS = 8 ?>
+        @for($file = 0; $file < FILES; $file++)
             <tr class="chessboard-row">
-            @for($j = 0; $j < SIZE; $j++)
+            @for($ranks = 0; $ranks < RANKS; $ranks++)
                 <td class="chessboard-cell"></td>
             @endfor
             </tr>
