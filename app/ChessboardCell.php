3c97abd6b728c07d525909335bd8cd77846a09ad
caco <caco@member.fsf.org>
2016-08-23 17:26:12 -0300

    Refatora para melhorar a bizarrice de antes

--- app/ChessboardCell.php
+++ app/ChessboardCell.php
@@ -12,167 +12,35 @@ class ChessboardCell extends Model
     const SECOND_ROW_BLACK = 7;
     const FIRST_ROW_WHITE = 1;
     const SECOND_ROW_WHITE = 2;
+    const FINAL_FILE = 'h';
+    const FINAL_RANK = 8;
 
     /**
      * Initialize pieces in chessboard.
      */
     public function initializePiecesOnChessboard()
     {
-        // Get pieces
-        // TODO: make a loop!
-        $blackPawns = $this->getPieces('pawn', 'black');
-        $whitePawns = $this->getPieces('pawn', 'white');
-        $blackRooks = $this->getPieces('rook', 'black');
-        $whiteRooks = $this->getPieces('rook', 'white');
-        $blackKnights = $this->getPieces('knight', 'black');
-        $whiteKnights = $this->getPieces('knight', 'white');
-        $blackBishops = $this->getPieces('bishop', 'black');
-        $whiteBishops = $this->getPieces('bishop', 'white');
-        $blackQueen = $this->getPieces('queen', 'black');
-        $whiteQueen = $this->getPieces('queen', 'white');
-        $blackKing = $this->getPieces('king', 'black');
-        $whiteKing = $this->getPieces('king', 'white');
+        $pieces = ChessPiece::all()->toArray();
 
-        // Put pieces on initial board positions
-        $this->putPieces('pawn', 'black', $blackPawns);
-        $this->putPieces('pawn', 'white', $whitePawns);
-        $this->putPieces('rook', 'black', $blackRooks);
-        $this->putPieces('rook', 'white', $whiteRooks);
-        $this->putPieces('knight', 'black', $blackKnights);
-        $this->putPieces('knight', 'white', $whiteKnights);
-        $this->putPieces('bishop', 'black', $blackBishops);
-        $this->putPieces('bishop', 'white', $whiteBishops);
-        $this->putPieces('queen', 'black', $blackQueen);
-        $this->putPieces('queen', 'white', $whiteQueen);
-        $this->putPieces('king', 'black', $blackKing);
-        $this->putPieces('king', 'white', $whiteKing);
-    }
-
-
-    /**
-     * Get player's pieces.
-     *
-     * @param $piece
-     * @param $player
-     * @return array
-     */
-    public function getPieces($piece, $player)
-    {
-        $isBlack = $player == 'black' ? true : false;
-
-        $quantity = self::INTERMEDIATE_PIECES; // initializes with common case
-
-        switch ($piece) {
-            case 'pawn':
-                $codePrefix = 'P';
-                $quantity = self::PAWN_PIECES;
-                break;
-            case 'rook':
-                $codePrefix = 'R';
-                break;
-            case 'knight':
-                $codePrefix = 'K';
-                break;
-            case 'bishop':
-                $codePrefix = 'B';
-                break;
-            case 'king':
-                return ChessPiece::where('code', 'K')->where('is_black', $isBlack)->get();
-            case 'queen':
-                return ChessPiece::where('code', 'Q')->where('is_black', $isBlack)->get();
-            default:
-                // TODO: Generates exception (?)
-        }
-
-        $pieces = [];
-        for ($pieceNumber = 1; $pieceNumber <= $quantity; $pieceNumber++) {
-            $pieces[] = ChessPiece::where('code', $codePrefix . $pieceNumber) //TODO: verificar se deve corrigir $codePrefix
-            ->where('is_black', $isBlack)
-                ->first();
-        }
-
-        return $pieces;
-    }
-
-    /**
-     * Put pieces in initial positions
-     *
-     * @param $piece
-     * @param $player
-     * @param $piecesArray
-     */
-    private function putPieces($piece, $player, $piecesArray)
-    {
-        // Defines rank to put pieces
-        if ($piece == 'pawn') {
-            // TODO: melhor abordagem aqui? Está passando se $player é qualquer coisa menos 'white'
-            $rank = self::SECOND_ROW_BLACK;
-            if ($player == 'white') {
-                $rank = self::SECOND_ROW_WHITE;
-            }
-        } else {
-            // TODO: melhor abordagem aqui? Está passando se $player é qualquer coisa menos 'white'
-            $rank = self::FIRST_ROW_BLACK;
-            if ($player == 'white') {
-                $rank = self::FIRST_ROW_WHITE;
+        // Put black pieces on board
+        for ($rank = self::FIRST_ROW_BLACK; $rank >= self::SECOND_ROW_BLACK; $rank--) {
+            for ($file = 'a'; $file <= self::FINAL_FILE; $file++) {
+                $piece = array_shift($pieces);
+                $cell = $this->where('file', $file)->where('rank', $rank)->first();
+                $cell->current_piece = $piece['id'];
+                $cell->save();
             }
         }
 
-        // Put pieces on board
-        if ($piece == 'pawn') {
-            for ($file = 'a'; $file <= 'h'; $file++) {
-                $pawnPiece = array_shift($piecesArray);
-                $this->putPieceOnBoard($file, $rank, $pawnPiece);
+        // Put white pieces on board
+        for ($rank = self::SECOND_ROW_WHITE; $rank >= self::FIRST_ROW_WHITE; $rank--) {
+            for ($file = 'a'; $file <= self::FINAL_FILE; $file++) {
+                $piece = array_shift($pieces);
+                $cell = $this->where('file', $file)->where('rank', $rank)->first();
+                $cell->current_piece = $piece['id'];
+                $cell->save();
             }
         }
-        if ($piece == 'rook') {
-            $file = 'a';
-            foreach ($piecesArray as $rookPiece) {
-                $this->putPieceOnBoard($file, $rank, $rookPiece);
-                $file = 'h';
-            }
-        }
-        if ($piece == 'knight') {
-            $file = 'b';
-            foreach ($piecesArray as $knightPiece) {
-                $this->putPieceOnBoard($file, $rank, $knightPiece);
-                $file = 'g';
-            }
-        }
-        if ($piece == 'bishop') {
-            $file = 'c';
-            foreach ($piecesArray as $bishopPiece) {
-                $this->putPieceOnBoard($file, $rank, $bishopPiece);
-                $file = 'f';
-            }
-        }
-        if ($piece == 'queen') {
-            $this->putPieceOnBoard('d', $rank, $piecesArray[0]);
-        }
-        if ($piece == 'king') {
-            $this->putPieceOnBoard('e', $rank, $piecesArray[0]);
-        }
-    }
-
-    /**
-     * Put one piece in board cell
-     *
-     * @param $file
-     * @param $rank
-     * @param $piece
-     */
-    public function putPieceOnBoard($file, $rank, $piece)
-    {
-        $chessboardCell = $this->where('file', $file)
-            ->where('rank', $rank)
-            ->first();
-
-        if (is_null($chessboardCell)) {
-            // Gera excessão: não pegou a célula
-        }
-        $chessboardCell->current_piece = $piece->id;
-
-        $chessboardCell->save();
     }
 
     /**
